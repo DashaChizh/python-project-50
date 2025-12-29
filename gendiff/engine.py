a80b257b1693f3f410b05e3d2d63ceeb868e8f22
Daria_Sotnikova <DashaChizh@gmail.com>
2025-12-29 15:31:08 +0300

    add -f option

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -5,9 +5,13 @@ def parse_args():
         prog="gendiff",
         description="Compares two configuration files and shows a difference."
         )
+    parser.add_argument(
+        '-f', '--format',
+        help='set format of output')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
     args = parser.parse_args()
+    parser.print_help
     print(f"{args.first_file}, {args.second_file}")
     return args.first_file, args.second_file
 
