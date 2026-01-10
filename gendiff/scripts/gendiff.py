f81a59e8c7d637b06a8fff05a87ccbc1ccc85806
Daria_Sotnikova <DashaChizh@gmail.com>
2026-01-10 10:24:58 +0300

    fix lint

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,5 @@
 import argparse
+
 from gendiff.gendiff import generate_diff
 
 
@@ -19,6 +20,7 @@ def main():
     args = parser.parse_args()
     diff = generate_diff(args.first_file, args.second_file, args.format)
     print(diff)
+
 
 if __name__ == "__main__":
     main()
