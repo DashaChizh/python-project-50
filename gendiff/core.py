cf1b6c22d5eba59cdadbd652d4ca2ad18571cc60
Daria_Sotnikova <DashaChizh@gmail.com>
2026-01-10 23:38:07 +0300

    add yaml support

--- gendiff/core.py
+++ gendiff/core.py
@@ -1,38 +1,18 @@
 import json
 
-'''def compare_datas(data1, data2):
-    result = []
-    all_keys = sorted(set(data1.keys()) | set(data2.keys()))
-            
-    for key in all_keys:
-        value1 = data1.get(key)
-        value2 = data2.get(key)
+import yaml
 
-        if key in data1 and key not in data2:
-            result.append(f"  - {key}: {format_value(value1)}")
-        elif key not in data1 and key in data2:
-            result.append(f"  + {key}: {format_value(value2)}")
-        elif value1 == value2:
-            result.append(f"    {key}: {format_value(value1)}")
-        else:
-            result.append(f"  - {key}: {format_value(value1)}")
-            result.append(f"  + {key}: {format_value(value2)}")
 
-    return result
-    
-
-def format_value(value):
-    if isinstance(value, bool):
-        return str(value).lower()
-    # elif value is None:
-        # return 'null'
-    else:
-        return str(value)'''    
+def load_data(filepath):
+    if filepath.endswith('.json'):
+        return json.load(open(filepath))
+    if filepath.endswith(('.yaml', '.yml')):
+        return yaml.safe_load(open(filepath))
 
 
 def generate_diff(filepath1, filepath2):
-    data1 = json.load(open(filepath1))
-    data2 = json.load(open(filepath2))
+    data1 = load_data(filepath1)
+    data2 = load_data(filepath2)
 
     result = []
     all_keys = sorted(set(data1.keys()) | set(data2.keys()))
